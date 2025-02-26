270e3dfb2f120c07ff605438b7c30c0b0ef24206
Gabriel Duval <gduvalkieffer@gmail.com>
2025-02-26 14:07:31 +0100

    annotation but script based

---
+++ tutorials/scGPT_annotation.py
@@ -0,0 +1,60 @@
+'''sc-GPT tutorial for cell type annotation but using
+a python script rather than a jupyter notebook.
+
+Jupyter notebook version had too many kernel crashes
+due to the large size of the data.
+'''
+
+# Imports
+
+import copy
+import gc
+import json
+import os
+from pathlib import Path
+import shutil
+import sys
+import time
+import traceback
+from typing import List, Tuple, Dict, Union, Optional
+import warnings
+import pandas as pd
+# from . import asyn
+import pickle
+import torch
+from anndata import AnnData
+import scanpy as sc
+import scvi
+import seaborn as sns
+import numpy as np
+import wandb
+from scipy.sparse import issparse
+import matplotlib.pyplot as plt
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
+from torchtext.vocab import Vocab
+from torchtext._torchtext import (
+    Vocab as VocabPybind,
+)
+from sklearn.metrics import confusion_matrix
+
+sys.path.insert(0, "../")
+import scgpt as scg
+from scgpt.model import TransformerModel, AdversarialDiscriminator
+from scgpt.tokenizer import tokenize_and_pad_batch, random_mask_value
+from scgpt.loss import (
+    masked_mse_loss,
+    masked_relative_error,
+    criterion_neg_log_bernoulli,
+)
+from scgpt.tokenizer.gene_tokenizer import GeneVocab
+from scgpt.preprocess import Preprocessor
+from scgpt import SubsetsBatchSampler
+from scgpt.utils import set_seed, category_str2int, eval_scib_metrics
+
+sc.set_figure_params(figsize=(6, 6))
+os.environ["KMP_WARNINGS"] = "off"
+warnings.filterwarnings('ignore')

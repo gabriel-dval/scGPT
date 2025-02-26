60fe1fab9f990066c350e111e706a9b8ba0d8873
Gabriel Duval <gduvalkieffer@gmail.com>
2025-02-26 15:31:19 +0100

    created script version of jupyter

--- tutorials/scGPT_annotation.py
+++ tutorials/scGPT_annotation.py
@@ -58,3 +58,308 @@ from scgpt.utils import set_seed, category_str2int, eval_scib_metrics
 sc.set_figure_params(figsize=(6, 6))
 os.environ["KMP_WARNINGS"] = "off"
 warnings.filterwarnings('ignore')
+
+
+def main():
+    '''Draft main function script to run all jupyter kernels
+    that aren't functions.
+
+    Args
+    ----
+    None
+
+    Returns
+    -------
+    None
+    '''
+
+    # Hyperparameters #################################################################
+    hyperparameter_defaults = dict(
+        seed=0,
+        dataset_name="sbm",
+        do_train=True,
+        load_model="../save/scGPT_human",
+        mask_ratio=0.0,
+        epochs=10,
+        n_bins=51,
+        MVC=False, # Masked value prediction for cell embedding
+        ecs_thres=0.0, # Elastic cell similarity objective, 0.0 to 1.0, 0.0 to disable
+        dab_weight=0.0,
+        lr=1e-4,
+        batch_size=32,
+        layer_size=128,
+        nlayers=4,  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+        nhead=4,  # number of heads in nn.MultiheadAttention
+        dropout=0.2,  # dropout probability
+        schedule_ratio=0.9,  # ratio of epochs for learning rate schedule
+        save_eval_interval=5,
+        fast_transformer=True,
+        pre_norm=False,
+        amp=True,  # Automatic Mixed Precision
+        include_zero_gene = False,
+        freeze = False, #freeze
+        DSBN = False,  # Domain-spec batchnorm
+        )
+    
+    # Initialize wandb #################################################################
+    run = wandb.init(
+        config=hyperparameter_defaults,
+        project="scGPT",
+        reinit=True,
+        settings=wandb.Settings(start_method="fork"),
+    )
+    config = wandb.config
+    print(config)
+
+    # Seed
+    set_seed(config.seed)
+
+
+    # Model settings #################################################################
+    # Settings for input and preprocessing
+    pad_token = "<pad>"
+    special_tokens = [pad_token, "<cls>", "<eoc>"]
+    mask_ratio = config.mask_ratio
+    mask_value = "auto"  # for masked values, now it should always be auto
+
+    include_zero_gene = config.include_zero_gene  # if True, include zero genes 
+                                                  # among hvgs in the training
+    max_seq_len = 3001
+    n_bins = config.n_bins
+
+    # input/output representation
+    input_style = "binned"  # "normed_raw", "log1p", or "binned"
+    output_style = "binned"  # "normed_raw", "log1p", or "binned"
+
+    # settings for training
+    MLM = False  # whether to use masked language modeling, currently it is always on.
+    CLS = True  # celltype classification objective
+    ADV = False  # Adversarial training for batch correction
+    CCE = False  # Contrastive cell embedding objective
+    MVC = config.MVC  # Masked value prediction for cell embedding
+    ECS = config.ecs_thres > 0  # Elastic cell similarity objective
+    DAB = False  # Domain adaptation by reverse backpropagation, set to 2 for separate optimizer
+    INPUT_BATCH_LABELS = False  # TODO: have these help MLM and MVC, while not to classifier
+    input_emb_style = "continuous"  # "category" or "continuous" or "scaling"
+    cell_emb_style = "cls"  # "avg-pool" or "w-pool" or "cls"
+    adv_E_delay_epochs = 0  # delay adversarial training on encoder for a few epochs
+    adv_D_delay_epochs = 0
+    mvc_decoder_style = "inner product"
+    ecs_threshold = config.ecs_thres
+    dab_weight = config.dab_weight
+
+    explicit_zero_prob = MLM and include_zero_gene  # whether explicit bernoulli for zeros
+    do_sample_in_train = False and explicit_zero_prob  # sample the bernoulli in training
+
+    per_seq_batch_sample = False
+
+    # settings for optimizer
+    lr = config.lr  # TODO: test learning rate ratio between two tasks
+    lr_ADV = 1e-3  # learning rate for discriminator, used when ADV is True
+    batch_size = config.batch_size
+    eval_batch_size = config.batch_size
+    epochs = config.epochs
+    schedule_interval = 1
+
+    # settings for the model
+    fast_transformer = config.fast_transformer
+    fast_transformer_backend = "flash"  # "linear" or "flash"
+    embsize = config.layer_size  # embedding dimension
+    d_hid = config.layer_size  # dimension of the feedforward network in TransformerEncoder
+    nlayers = config.nlayers  # number of TransformerEncoderLayer in TransformerEncoder
+    nhead = config.nhead  # number of heads in nn.MultiheadAttention
+    dropout = config.dropout  # dropout probability
+
+    # logging
+    log_interval = 100  # iterations
+    save_eval_interval = config.save_eval_interval  # epochs
+    do_eval_scib_metrics = True
+
+
+    # Validate settings #################################################################
+    assert input_style in ["normed_raw", "log1p", "binned"]
+    assert output_style in ["normed_raw", "log1p", "binned"]
+    assert input_emb_style in ["category", "continuous", "scaling"]
+    if input_style == "binned":
+        if input_emb_style == "scaling":
+            raise ValueError("input_emb_style `scaling` is not supported for binned input.")
+    elif input_style == "log1p" or input_style == "normed_raw":
+        if input_emb_style == "category":
+            raise ValueError(
+                "input_emb_style `category` is not supported for log1p or normed_raw input."
+            )
+
+    if input_emb_style == "category":
+        mask_value = n_bins + 1
+        pad_value = n_bins  # for padding gene expr values
+        n_input_bins = n_bins + 2
+    else:
+        mask_value = -1
+        pad_value = -2
+        n_input_bins = n_bins
+
+    if ADV and DAB:
+        raise ValueError("ADV and DAB cannot be both True.")
+    DAB_separate_optim = True if DAB > 1 else False
+
+
+    # Prep save directory #################################################################
+    dataset_name = config.dataset_name
+    save_dir = Path(f"./save/dev_{dataset_name}-{time.strftime('%b%d-%H-%M')}/")
+    save_dir.mkdir(parents=True, exist_ok=True)
+    print(f"save to {save_dir}")
+    logger = scg.logger
+    scg.utils.add_file_handler(logger, save_dir / "run.log")
+
+
+    # Load data #################################################################
+    if dataset_name == "sbm":
+        # Load the reference and query datasets
+        data_dir = Path("../data/sbm")
+        adata = sc.read(data_dir / "mATLAS_Marrow_droplet.h5ad")
+        adata_test = sc.read(data_dir / "mATLAS_Marrow_facs.h5ad")
+
+        # Keep only common genes between both datasets
+        common_genes = adata.var_names.intersection(adata_test.var_names)
+        adata = adata[:, common_genes]
+        adata_test = adata_test[:, common_genes]
+
+        # Set the cell type as the label and add batch id
+        adata.obs["celltype"] = adata.obs["free_annotation"].astype("category")
+        adata_test.obs["celltype"] = adata_test.obs["free_annotation"].astype("category")
+        adata.obs["batch_id"] = adata.obs["str_batch"] = "0"
+        adata_test.obs["batch_id"]  = adata_test.obs["str_batch"] = "1"  
+
+        # Add gene names as features 
+        # The gene names need to be capitalised for human to mouse conversion
+        adata.var['gene_name'] = adata.var['n_cells'].index
+        adata.var['gene_name'] = adata.var['gene_name'].str.upper()
+        adata_test.var['gene_name'] = adata_test.var['n_cells'].index
+        adata_test.var['gene_name'] = adata_test.var['gene_name'].str.upper()
+
+        # Set these as index
+        adata.var.set_index(adata.var["gene_name"], inplace=True)
+        adata_test.var.set_index(adata.var["gene_name"], inplace=True)
+        data_is_raw = False
+        filter_gene_by_counts = False
+        adata_test_raw = adata_test.copy()
+        adata = adata.concatenate(adata_test, batch_key="str_batch")
+                    
+    # make the batch category column
+    batch_id_labels = adata.obs["str_batch"].astype("category").cat.codes.values
+    adata.obs["batch_id"] = batch_id_labels
+    celltype_id_labels = adata.obs["celltype"].astype("category").cat.codes.values
+    celltypes = adata.obs["celltype"].unique()
+    num_types = len(np.unique(celltype_id_labels))
+    id2type = dict(enumerate(adata.obs["celltype"].astype("category").cat.categories))
+    adata.obs["celltype_id"] = celltype_id_labels
+    adata.var["n_cells"] = adata.var.index.tolist()
+
+
+    # LOAD MODEL #################################################################
+    if config.load_model is not None:
+        model_dir = Path(config.load_model)
+        model_config_file = model_dir / "args.json"
+        model_file = model_dir / "best_model.pt"
+        vocab_file = model_dir / "vocab.json"
+
+        vocab = GeneVocab.from_file(vocab_file)
+        shutil.copy(vocab_file, save_dir / "vocab.json")
+        for s in special_tokens:
+            if s not in vocab:
+                vocab.append_token(s)
+
+        adata.var["id_in_vocab"] = [
+            1 if gene in vocab else -1 for gene in adata.var["gene_name"]
+        ]
+        gene_ids_in_vocab = np.array(adata.var["id_in_vocab"])
+        logger.info(
+            f"match {np.sum(gene_ids_in_vocab >= 0)}/{len(gene_ids_in_vocab)} genes "
+            f"in vocabulary of size {len(vocab)}."
+        )
+        adata = adata[:, adata.var["id_in_vocab"] >= 0]
+
+        # model
+        with open(model_config_file, "r") as f:
+            model_configs = json.load(f)
+        logger.info(
+            f"Resume model from {model_file}, the model args will override the "
+            f"config {model_config_file}."
+        )
+        embsize = model_configs["embsize"]
+        nhead = model_configs["nheads"]
+        d_hid = model_configs["d_hid"]
+        nlayers = model_configs["nlayers"]
+        n_layers_cls = model_configs["n_layers_cls"]
+
+
+    # Preprocess #################################################################
+    # set up the preprocessor, use the args to config the workflow
+    preprocessor = Preprocessor(
+        use_key="X",  # the key in adata.layers to use as raw data
+        filter_gene_by_counts=filter_gene_by_counts,  # step 1
+        filter_cell_by_counts=False,  # step 2
+        normalize_total=1e4,  # 3. whether to normalize the raw data and to what sum
+        result_normed_key="X_normed",  # the key in adata.layers to store the normalized data
+        log1p=data_is_raw,  # 4. whether to log1p the normalized data
+        result_log1p_key="X_log1p",
+        subset_hvg=False,  # 5. whether to subset the raw data to highly variable genes
+        hvg_flavor="seurat_v3" if data_is_raw else "cell_ranger",
+        binning=n_bins,  # 6. whether to bin the raw data and to what number of bins
+        result_binned_key="X_binned",  # the key in adata.layers to store the binned data
+    )
+
+    adata_test = adata[adata.obs["str_batch"] == "1"]
+    adata = adata[adata.obs["str_batch"] == "0"]
+
+    preprocessor(adata, batch_key=None)
+    preprocessor(adata_test, batch_key=None)
+
+
+    # Prepare data #################################################################
+    input_layer_key = {  # the values of this map correspond to the keys in preprocessing
+        "normed_raw": "X_normed",
+        "log1p": "X_normed",
+        "binned": "X_binned",
+    }[input_style]
+    all_counts = (
+        adata.layers[input_layer_key].A
+        if issparse(adata.layers[input_layer_key])
+        else adata.layers[input_layer_key]
+    )
+    genes = adata.var["gene_name"].tolist()
+
+    celltypes_labels = adata.obs["celltype_id"].tolist()  # make sure count from 0
+    celltypes_labels = np.array(celltypes_labels)
+
+    batch_ids = adata.obs["batch_id"].tolist()
+    num_batch_types = len(set(batch_ids))
+    batch_ids = np.array(batch_ids)
+
+    (
+        train_data,
+        valid_data,
+        train_celltype_labels,
+        valid_celltype_labels,
+        train_batch_labels,
+        valid_batch_labels,
+    ) = train_test_split(
+        all_counts, celltypes_labels, batch_ids, test_size=0.1, shuffle=True
+    )
+
+    # Vocab step
+    if config.load_model is None:
+        vocab = Vocab(
+            VocabPybind(genes + special_tokens, None)
+        )  # bidirectional lookup [gene <-> int]
+    vocab.set_default_index(vocab["<pad>"])
+    gene_ids = np.array(vocab(genes), dtype=int)
+
+
+
+
+
+
+# Main script
+if __name__ == '__main__':
+    main()
